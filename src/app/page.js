e478055790cabfbd487464792a94e2e571f8fca0
jbrace817 <48393754+jbrace817@users.noreply.github.com>
2024-10-20 22:49:41 -0400

    Hero component created

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,14 @@
 import Image from 'next/image';
 import { Raleway } from 'next/font/google';
-const raleway = Raleway({
-  subsets: ['latin'],
-  weight: ['400', '700'],
-  display: 'swap',
-});
+import Hero from '@/components/Hero';
 
 export default function Home() {
-  return <div className="">test</div>;
+  return (
+    <div className="">
+      <Hero
+        heading="Captur Photography"
+        message="I capture moments in nature and keep them alive."
+      />
+    </div>
+  );
 }
