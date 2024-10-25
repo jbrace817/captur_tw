9d595ce52140967d65ae0b52b91c7093f09b1159
jbrace817 <48393754+jbrace817@users.noreply.github.com>
2024-10-24 21:54:59 -0400

    fix: Correct navbar
    Fixed link in nav as well as fixes the mobile nave to close once a link is clicked

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,7 +44,7 @@ function Navbar() {
             <Link href="/#gallery">Gallery</Link>
           </li>
           <li className="p-4">
-            <Link href="/portfolio">Work</Link>
+            <Link href="/work">Work</Link>
           </li>
           <li className="p-4">
             <Link href="/contact">Contact</Link>
@@ -65,16 +65,28 @@ function Navbar() {
           } right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300`}
         >
           <ul>
-            <li className="p-4 text-4xl hover:text-gray-500">
+            <li
+              onClick={handleNav}
+              className="p-4 text-4xl hover:text-gray-500"
+            >
               <Link href="/">Home</Link>
             </li>
-            <li className="p-4 text-4xl hover:text-gray-500">
+            <li
+              onClick={handleNav}
+              className="p-4 text-4xl hover:text-gray-500"
+            >
               <Link href="/#gallery">Gallery</Link>
             </li>
-            <li className="p-4 text-4xl hover:text-gray-500">
-              <Link href="/portfolio">Work</Link>
+            <li
+              onClick={handleNav}
+              className="p-4 text-4xl hover:text-gray-500"
+            >
+              <Link href="/work">Work</Link>
             </li>
-            <li className="p-4 text-4xl hover:text-gray-500">
+            <li
+              onClick={handleNav}
+              className="p-4 text-4xl hover:text-gray-500"
+            >
               <Link href="/contact">Contact</Link>
             </li>
           </ul>
